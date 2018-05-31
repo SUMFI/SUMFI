9d28c92240d2b849cc732c746fdfe4c4d5e1d855
pknut <paulina.knut@gmail.com>
2018-05-31 19:37:53 +0200

    Add prepare function

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import itertools
+import os
+import re
+
 import click
+import pandas as pd
 
 
 @click.group()
@@ -7,13 +12,18 @@ def action():
 
 
 @action.command()
-@click.option('-p', '--proteins_dir_path', help='Path to directory with fasta files', required=True)
-def prepare(proteins_dir_path):
-    _prepare(proteins_dir_path)
+@click.option('-p', '--proteins_dir', help='Path to folder with protein files', required=True)
+@click.option('-o', '--output_dir', help='Path to output folder', required=True)
+def prepare(proteins_dir, output_dir):
+    _prepare(proteins_dir, output_dir)
 
 
-def _prepare(proteins_dir_path):
-    pass
+def _prepare(proteins_dir, output_dir):
+    list_files = os.listdir(path=proteins_dir)
+    list_fasta = [file for file in list_files if re.match('.*\.fasta', file)]
+    fasta_combinations = list(itertools.combinations(list_fasta, 2))
+    protein_combinations_path = os.path.join(output_dir, 'protein_combinations.csv')
+    pd.DataFrame(fasta_combinations).to_csv(protein_combinations_path)
 
 
 @action.command()
@@ -26,7 +36,8 @@ def _run():
 
 
 @action.command()
-@click.option('-p', '--proteins_dir_path', help='Path to directory with fasta files', required=True)
+@click.option('-p', '--proteins_dir', help='Path to folder with protein files', required=True)
+@click.option('-o', '--output_dir', help='Path to output folder', required=True)
 def prepare_run(proteins_dir_path):
     _prepare(proteins_dir_path)
     _run()
