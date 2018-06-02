6368caae635a77fb65bcb7d4e5e697fb8ea6f88f
pknut <paulina.knut@gmail.com>
2018-06-02 15:55:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='biogner',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='paulina',
+    author_email='',
+    description=''
+)
